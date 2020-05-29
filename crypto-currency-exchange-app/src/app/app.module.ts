5bf9b55425c270440b7291a683a995bcc6c2e73b
dawid <dawidbronczak1993@gmail.com>
2020-05-29 12:15:18 +0200

    added material desing & reimplement views

--- crypto-currency-exchange-app/src/app/app.module.ts
+++ crypto-currency-exchange-app/src/app/app.module.ts
@@ -5,16 +5,33 @@ import { AppComponent } from './app.component';
 import { ExchangeRateComponent } from './exchange-rate/exchange-rate.component';
 import { ExchangeValueComponent } from './exchange-value/exchange-value.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ExchangeRateComponent,
     ExchangeValueComponent
-  ],  
+  ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule 
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
